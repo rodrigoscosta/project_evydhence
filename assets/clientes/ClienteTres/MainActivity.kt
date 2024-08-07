617fb9f7a18ce5634bc263738ffe7ced5d36aca4
Rodrigo Santos Costa <66096527+rodrigoscosta@users.noreply.github.com>
2024-08-07 09:05:17 -0300

    Alteração dos arquivos com seus nomes correspondentes

--- assets/clientes/ClienteTres/MainActivity.kt
+++ assets/clientes/ClienteTres/MainActivity.kt
@@ -1,4 +1,4 @@
-package br.com.sinqia.appconsorcio
+package br.com.clientetres.evydhence
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
--- assets/clientes/ClienteUm/MainActivity.kt
+++ assets/clientes/ClienteUm/MainActivity.kt
@@ -1,4 +1,4 @@
-package br.com.sinqia.appconsorcio
+package br.com.clienteum.evydhence
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
